a774cec46c4a79da1955cc4cf3b8031191864d00
Corie Stewart <coriestewart1989>
2021-07-21 11:40:43 -0400

    server up and running

--- api/server.js
+++ api/server.js
@@ -1,3 +1,21 @@
 const express = require("express")
 const helmet = require("helmet")
-const 
+const cors = require("cors")
+
+//routers
+
+
+const server = express()
+
+//middleware
+server.use(helmet())
+server.use(express.json())
+server.use(cors())
+
+//endpoints
+
+server.get("*", (req, res) =>{
+    res.send(`<h2>"Server running"</h2>`)
+})
+
+module.exports = server
